1a9757ba233e13850b12dd04b3aef6f3655fe464
shadorc <d.ceccarelli@outlook.fr>
2025-05-10 22:37:24 +0200

    Change cast endpoint to get and add response

--- rpitube-server.js
+++ rpitube-server.js
@@ -2,15 +2,17 @@ var exec = require('child_process').exec;
 var express = require('express');
 var app = express();
 
-app.post('/cast/:ip/:url', function (req, res) {
+app.get('/cast/:ip/:url', function (req, res) {
     const ip = req.params.ip;
     const url = req.params.url;
 
-    console.log(`Downloading at ${url}...`);
+    console.log(`Downloading video ${url}...`);
     exec(`youtube-dl ${url} -f mp4 -o video`);
 
-    console.log(`Casting video...`);
+    console.log(`Casting video to ${ip}...`);
     exec(`vlc video.mp4 -I http --http-password 'rpitube' --sout '#chromecast' --sout-chromecast-ip=${ip} --demux-filter=demux_chromecast`);
+
+    res.send(`Casting ${url} to Chromecast ${ip}`);
 });
 
 var srv = app.listen(3000, function () {
