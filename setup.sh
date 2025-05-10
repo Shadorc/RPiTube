9fb1c46d900d9d02c4b0c453009e027d838e3dcd
shadorc <d.ceccarelli@outlook.fr>
2025-05-10 22:35:22 +0200

    Download scripts in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,12 @@
 echo "Installing dependencies..."
 sudo apt-get install youtube-dl vlc node-express -y
 
+echo "Creating directory..."
+mkdir $HOME/RPiTube
+
+echo "Downloading scripts..."
+GIT_URL="https://raw.githubusercontent.com/Shadorc/RPiTube/master"
+wget -O $HOME/RPiTube/rpitube-server.js $GIT_URL/rpitube-server.js
+wget -O $HOME/RPiTube/start-server.sh $GIT_URL/start-server.sh
+
 sudo sh start-server.sh
