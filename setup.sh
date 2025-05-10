201b2125780431d722534de6cdd808ca4a8ad65c
shadorc <d.ceccarelli@outlook.fr>
2025-05-10 22:47:32 +0200

    Fix setup current directory

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ echo "Installing dependencies..."
 sudo apt-get install youtube-dl vlc node-express -y
 
 echo "Creating directory..."
-mkdir $HOME/RPiTube
+mkdir "RPiTube"
 
 echo "Downloading scripts..."
 GIT_URL="https://raw.githubusercontent.com/Shadorc/RPiTube/master"
-wget -O $HOME/RPiTube/rpitube-server.js $GIT_URL/rpitube-server.js
-wget -O $HOME/RPiTube/start-server.sh $GIT_URL/start-server.sh
+wget -O ./RPiTube/rpitube-server.js $GIT_URL/rpitube-server.js
+wget -O ./RPiTube/start-server.sh $GIT_URL/start-server.sh
 
 sudo sh start-server.sh
