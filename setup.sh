570af0a6f6d560797ba349d184ca3e6a3f4f48a2
shadorc <d.ceccarelli@outlook.fr>
2025-05-10 22:52:40 +0200

    Do not create sub-dir

--- setup.sh
+++ setup.sh
@@ -3,11 +3,8 @@
 echo "Installing dependencies..."
 sudo apt-get install youtube-dl vlc node-express -y
 
-echo "Creating directory..."
-DIR="${PWD}/RPiTube"
-mkdir $DIR
-
 echo "Downloading scripts..."
+DIR=`PWD`
 GIT_URL="https://raw.githubusercontent.com/Shadorc/RPiTube/master"
 wget -O $DIR/rpitube-server.js $GIT_URL/rpitube-server.js
 wget -O $DIR/start-server.sh $GIT_URL/start-server.sh
