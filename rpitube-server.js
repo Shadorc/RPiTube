ca4f81219f8d4f512fbd853f21647eca0cfdb1eb
shadorc <d.ceccarelli@outlook.fr>
2025-05-11 13:47:47 +0200

    Fix fail when video name contains spaces

--- rpitube-server.js
+++ rpitube-server.js
@@ -22,7 +22,7 @@ app.get('/cast/:ip/:url', function (req, res) {
 
     console.time('Downloading video');
     console.log(`Downloading video ${url}...`);
-    if (!execSyncSafe(`yt-dlp '${url}' -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o '${videos_dir}/%(title)s.%(ext)s' --merge-output-format mkv --print-to-file after_move:filepath '${video_filepath_file}'`)) {
+    if (!execSyncSafe(`yt-dlp "${url}" -f "bestvideo[height<=1080]+bestaudio/best[height<=1080]" -o "${videos_dir}/%(title)s.%(ext)s" --merge-output-format mkv --print-to-file after_move:filepath "${video_filepath_file}"`)) {
         console.log("Downloading video failed.");
         return;
     }
@@ -37,7 +37,7 @@ app.get('/cast/:ip/:url', function (req, res) {
     }
 
     console.log(`Casting video to ${ip}...`);
-    if (!execSyncSafe(`vlc '${video_filepath}' -I http --http-password 'rpitube' --sout '#chromecast' --sout-chromecast-ip=${ip} --demux-filter=demux_chromecast --play-and-exit`)) {
+    if (!execSyncSafe(`vlc "${video_filepath}" -I http --http-password "rpitube" --sout "#chromecast" --sout-chromecast-ip=${ip} --demux-filter=demux_chromecast --play-and-exit`)) {
         console.log("Casting video failed.");
         return;
     }
